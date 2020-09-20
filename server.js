232629a6dec6959aa0e67cbebcdf009374e8756a
raiders032 <nameks@naver.com>
2020-09-20 21:26:50 +0900

    dockerfile

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const PORT = 8080;
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!!!!!!!!!!!!!!!!!");
 });
 
 app.listen(PORT);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Running on http://${PORT}`);
